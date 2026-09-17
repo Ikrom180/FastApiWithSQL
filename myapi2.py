385694caf19136456e3c9b48c1641d986c7f9fea
ikrom724 <ikrompersonal@gmail.com>
2026-09-17 17:38:07 +0500

    Sql with Fast APi finished

--- myapi2.py
+++ myapi2.py
@@ -31,7 +31,7 @@ Base.metadata.create_all(engine)
 class UserCreate(BaseModel):
     username: str
     email: str
-    password: str
+    role: str
 
 class UserResponse(BaseModel):
     id:int
@@ -43,6 +43,73 @@ class UserResponse(BaseModel):
         from_attributes = True
 
 
+class UpdateUserResponse(BaseModel):
+    message: str
+    user: UserResponse
+
+    class Config:
+        from_attributes = True   # Pydantic v2 (use orm_mode=True in v1)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+get_db()
+
+
 @app.get("/", tags=["root"])
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/users/{user_id}", tags=["Users"], response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@app.post("/users/", tags=["Users"], response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    if db.query(User).filter(User.email == user.email).first():
+        raise HTTPException(status_code=404, detail="Email already exists")
+    if '@' not in user.email:
+        raise HTTPException(status_code=400, detail="Wrong email")
+
+    #create a new user
+    new_user = User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+#user update
+@app.put("/users/{user_id}", tags=["Users"], response_model=UpdateUserResponse)
+def update_user(user_id: int, user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if '@' not in user.email:
+        raise HTTPException(status_code=400, detail="Wrong email", )
+
+    for field, value in user.dict().items():
+        setattr(db_user, field, value)
+
+    db.commit()
+    db.refresh(db_user)
+    # print(user.model_dump())
+    return {"message": "User updated", "user": db_user}
+
+@app.delete("/users/{user_id}", tags=["Users"])
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(db_user)
+    db.commit()
+    return {"message": "User deleted", "user": db_user}
